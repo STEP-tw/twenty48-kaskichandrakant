0db61dc1a5e9c43cc458743de4556255d5a3f9b2
kaskichandrakant <chandk@thoughtworks.com>
2019-02-22 18:16:19 +0530

    added the up-down-left-right movements for 2048

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,40 @@
 (ns twenty48.core
   (:gen-class))
 
+(def four-times-zero (repeat 4 0))
+(def remove-zero (partial remove zero?))
+(def rotate-90 (partial apply map list))
+
+(def move-right (comp (partial take-last 4)
+                      (partial concat (repeat 4 0))
+                      (partial map (partial apply +))
+                      (partial mapcat (partial partition-all 2))
+                      (partial partition-by identity)
+                      (partial remove-zero)))
+(def move-left (comp (partial reverse) (partial move-right) (partial reverse)))
+
+(def move-up
+  (comp rotate-90 (partial map (partial move-left)) (partial apply (partial map list))))
+
+(def move-down
+ (comp rotate-90 (partial map (partial move-right)) (partial apply (partial map list))))
+
 (defn move-grid-right
   "Moves an entire grid to the right"
   [grid]
-  grid)
+  (map move-right grid))
 
 (defn move-grid-left
   "Moves an entire grid to the left"
   [grid]
-  grid)
+  (map move-left grid))
 
 (defn move-grid-down
   "Moves an entire grid down"
   [grid]
-  grid)
+  (move-down grid))
 
 (defn move-grid-up
   "Moves an entire grid up"
   [grid]
-  grid)
+  (move-up grid))
